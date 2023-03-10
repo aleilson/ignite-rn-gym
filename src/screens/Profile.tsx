8f5a9dab7f5ab764c19fb21a042d444f10b44cec
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2023-03-10 08:23:12 -0300

    added imagePicker to change image profile and fileSystem to get photo size

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, useToast, VStack } from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Button } from "@components/Button";
@@ -10,6 +12,50 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/aleilson.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+      });
+  
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: 'Essa image é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: 'Não foi possível alterar sua imagem, tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -27,13 +73,13 @@ export function Profile() {
             />
             :
             <UserPhoto
-              source={{ uri: 'https://github.com/aleilson.png'}}
+              source={{ uri: userPhoto}}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
